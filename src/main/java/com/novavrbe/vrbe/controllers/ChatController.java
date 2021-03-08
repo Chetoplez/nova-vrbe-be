6f6c1c14b371827e745b763d46971b462b4069ce
Branci89 <cbranci89@gmail.com>
2021-03-08 14:06:43 +0100

    Aggiunta la route per eliminare il messaggio dalla chat. Manca la gestione della permission.

--- src/main/java/com/novavrbe/vrbe/controllers/ChatController.java
+++ src/main/java/com/novavrbe/vrbe/controllers/ChatController.java
@@ -3,6 +3,8 @@ package com.novavrbe.vrbe.controllers;
 import com.novavrbe.vrbe.business.ChatBusiness;
 import com.novavrbe.vrbe.models.chatcontroller.AddMessageRequest;
 import com.novavrbe.vrbe.models.chatcontroller.AddMessageResponse;
+import com.novavrbe.vrbe.models.chatcontroller.DeleteMessageRequest;
+import com.novavrbe.vrbe.models.chatcontroller.DeleteMessageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,11 @@ public class ChatController {
     @PostMapping("/addmessage")
     public ResponseEntity<AddMessageResponse> addMessage(@RequestBody AddMessageRequest request){
         return chatBusiness.addMessage(request);
+    }
+
+    @DeleteMapping("/deleteMessage")
+    public ResponseEntity<DeleteMessageResponse> deleteMessage(DeleteMessageRequest request){
+        return  chatBusiness.deleteMessage(request);
     }
 
 }
