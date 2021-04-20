e96cdce020c6655c0269e08c8fa8db65906abd72
ClaudioB <cbranci89@gmail.com>
2021-04-20 17:39:41 +0200

    Aggiunto al controller il metodo per spostarti da un luogo all'altro. creato il PresentiLuogoRepositoryService per poter interagire con il DB

--- src/main/java/com/novavrbe/vrbe/controllers/PresentiController.java
+++ src/main/java/com/novavrbe/vrbe/controllers/PresentiController.java
@@ -2,12 +2,13 @@ package com.novavrbe.vrbe.controllers;
 
 
 import com.novavrbe.vrbe.business.PresentiBusiness;
+import com.novavrbe.vrbe.models.presenticontroller.MoveToLuogoRequest;
+import com.novavrbe.vrbe.models.presenticontroller.MoveToLuogoResponse;
+import com.novavrbe.vrbe.models.presenticontroller.PresentiChatResponse;
 import com.novavrbe.vrbe.models.presenticontroller.PresentiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/presenti")
@@ -19,5 +20,15 @@ public class PresentiController {
     ResponseEntity<PresentiResponse> getPresenti(){
         return presentiBusiness.getPresenti();
 
+    }
+
+    @GetMapping("/presenti/chatId={chatId}")
+    public ResponseEntity<PresentiChatResponse> getPresentiChat(@PathVariable String chatId){
+        return presentiBusiness.getPresentiChat(chatId);
+    }
+
+    @PatchMapping("/moveto")
+    public ResponseEntity<MoveToLuogoResponse> moveToLuogo(@RequestBody MoveToLuogoRequest request){
+        return presentiBusiness.moveToLuogo(request);
     }
 }
---
+++ src/main/java/com/novavrbe/vrbe/repositories/PresentiLuogoRepository.java
@@ -0,0 +1,7 @@
+package com.novavrbe.vrbe.repositories;
+
+import com.novavrbe.vrbe.dto.PresentiLuogoDto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PresentiLuogoRepository extends CrudRepository<PresentiLuogoDto, Integer> {
+}
