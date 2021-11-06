eefea5652cb27d9e398fe4868c7868cd85b30168
ClaudioB <cbranci89@gmail.com>
2021-11-06 18:59:32 +0100

    configurazioni per rilascio funzionante

--- src/main/java/com/novavrbe/vrbe/VrbeApplication.java
+++ src/main/java/com/novavrbe/vrbe/VrbeApplication.java
@@ -3,14 +3,17 @@ package com.novavrbe.vrbe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableScheduling
-public class VrbeApplication {
+public class VrbeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VrbeApplication.class, args);
 	}
+
+
 
 }
