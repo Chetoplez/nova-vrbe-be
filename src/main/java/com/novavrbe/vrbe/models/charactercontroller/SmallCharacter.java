ad88b3c85afd36679eebb645b5b524fd5136a8e5
ClaudioB <cbranci89@gmail.com>
2021-11-13 18:17:49 +0100

    classe SmallCharacter per la lista degli utenti per le dropDown

---
+++ src/main/java/com/novavrbe/vrbe/models/charactercontroller/SmallCharacter.java
@@ -0,0 +1,10 @@
+package com.novavrbe.vrbe.models.charactercontroller;
+
+import lombok.Data;
+
+@Data
+public class SmallCharacter {
+    private Integer characterId;
+    private String characterName;
+    private String characterImg;
+}
