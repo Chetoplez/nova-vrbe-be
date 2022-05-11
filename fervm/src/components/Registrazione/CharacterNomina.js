4562c5c4c10f938a289bcf794073966be9d763c1
ClaudioB <cbranci89@gmail.com>
2022-05-11 11:36:50 +0200

    new set of buttons

---
+++ fervm/src/components/Registrazione/CharacterNomina.js
@@ -0,0 +1,38 @@
+import { TextField } from "@material-ui/core";
+import React from "react";
+
+function CharacterNomina({ errors, register}){
+    return(
+        <div style={{ marginTop: '30px' }} >
+                <h4>Inserisci il tuo Nomen</h4>
+                <div style={{ display: 'flex', alignItems: 'end', justifyContent: 'center' }}>
+                    <TextField
+                        placeholder="Inserisci il tuo Nomen"
+                        name="characterName"
+                        {...register("characterName", { 
+                            required: {value:true, message: 'Questo campo è obbligatorio'},
+                            minLength: {value: 3, message: 'il Nome deve essere almeno 3 caratteri'},
+                            maxLength: {value: 25, message: 'Il nome non deve superare i 25 caratteri'}})}
+                     />
+                    {/* <IconButton
+                        style={{ borderRadius: '50%', backgroundColor: '#db872a', marginLeft: '20px' }}>
+                        <CasinoOutlinedIcon style={{ color: 'white' }}></CasinoOutlinedIcon>
+                    </IconButton> */}
+                </div>
+                <p className="errorMessage">{errors.characterName?.type === 'required' ? errors.characterName.message : ''}</p>
+                <p className="errorMessage">{errors.characterName?.type === 'minLength' ? errors.characterName.message : ''}</p>
+                <p className="errorMessage">{errors.characterName?.type === 'maxLength' ? errors.characterName.message : ''}</p>
+                <h4>Inserisci il tuo Cognomen</h4>
+                <TextField
+                    placeholder="Inserisci il Cognomen"
+                    name="characterCognomen"
+                    {...register("characterCognomen", { 
+                            required: {value:true, message: 'Questo campo è obbligatorio'},
+                            minLength: {value: 3, message: 'il Nome deve essere almeno 3 caratteri'},
+                            maxLength: {value: 25, message: 'Il nome non deve superare i 25 caratteri'}} )} />
+                <p className="errorMessage">{errors.characterCognomen?.type === 'required' ? errors.characterCognomen.message : ''}</p>
+                <p className="errorMessage">{errors.characterCognomen?.type === 'minLength' ? errors.characterCognomen.message : ''}</p>
+                <p className="errorMessage">{errors.characterCognomen?.type === 'maxLength' ? errors.characterCognomen.message : ''}</p>
+            </div>
+    )
+} export default CharacterNomina;
